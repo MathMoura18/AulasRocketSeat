6cf9b08a5d3c2731737a5c46f0b813cefa7a4e7d
Matheus Moura <mathmo.ura242@gmail.com>
2023-10-07 02:49:31 -0300

    initial commmit

---
+++ aula7/script.js
@@ -0,0 +1,22 @@
+//Variáveis: let (alterável) e const (nao-alterável)
+//function = função
+
+function quantPernas(pernas) {
+    if(pernas == 2){
+        alert("O animal é um SER HUMANO!")
+    }
+    else if (pernas == 4){
+        alert("O animal é um CACHORRO!")
+    }
+    else if (pernas == 6){
+        alert("O animal é uma FORMIGA!")
+    }
+    else if (pernas == 8){
+        alert("O animal é uma ARANHA!")
+    }
+    else {
+        alert("O animal é desconhecido.")
+    }
+};
+
+quantPernas();
